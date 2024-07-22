1fabb00a5d973194f699c3aec192277d23683b37
Tarun Sharma <tarunshrm768@gmail.com>
2024-07-22 13:08:11 +0530

    commit

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -2,7 +2,7 @@
 
 const Footer = () => {
   return (
-    <Box component="footer" sx={{ backgroundColor: '#f8f8f8', padding: 2, borderTop: '1px solid #e7e7e7', textAlign: 'center', position: 'sticky',
+    <Box component="footer" sx={{ backgroundColor: '#f8f8f8', padding: 2, borderTop: '1px solid #e7e7e7', textAlign: 'center',
         bottom: 0 }}>
       <Typography variant="body2" color="textSecondary">
         &copy; {new Date().getFullYear()} Developed By Tarun Sharma
